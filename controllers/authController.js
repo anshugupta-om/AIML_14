c18d7b0eb2b946c4be7e4a999506f5d544dede39
Anshu_Gupta <anshugupta2307@gmail.com>
2026-04-14 15:24:30 +0530

    login work implemented

--- controllers/authController.js
+++ controllers/authController.js
@@ -44,4 +44,54 @@ const registerUser =async(req,res)=> {
     }
 };
 
-module.exports ={ registerUser};
+const loginUser = async(req,res)=>{
+    try{
+        const {email,password} =req.body;
+        if(!email || !password){
+            return res.status(400).json({
+                success:false,
+                message: "Email and password required"
+            });
+        }
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({
+                success:false,
+                message: "Invalid "
+            });
+        }
+        const isMatch = await bcrypt.compare(password,user.password);
+        if (!isMatch){
+            return res.status(404).json({
+                success: false,
+                message:" Invalid Password"
+            });
+        }
+        const token = jwt.sign(
+            {id: user._id},
+            process.env.JWT_SECRET,
+            { expiresIn: "7d"}
+        );
+        res.status(200).json({
+            success:true,
+            message:"Login Success",
+            token,
+            user:{
+                id: user._id,
+                name: user.name,
+                email: user.email
+            }
+
+        });
+    }
+    catch(e){
+        res.status(500).json({
+            success:false,
+            message:"Unable to login",
+            error: e.message
+        })
+         
+    }
+};
+
+module.exports ={ registerUser, loginUser};
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,9 +2,9 @@ const express = require("express");
 const User =require("../models/User");
 const bcrypt =require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const { registerUser } = require("../controllers/authController");
+const { registerUser, loginUser } = require("../controllers/authController");
 const router = express.Router();
 
-router.post("/register", registerUser)
-
+router.post("/register", registerUser);
+router.post("/login",loginUser);
 module.exports =router;
